6d065838ce2c1d806c0e8475b65498b4c46e27c6
Dana Volovelsky <danavolovelsky@MacBook-Pro-von-Dana.local>
2024-01-24 15:42:09 +0000

    remove textures

--- src/createWalls.jsx
+++ src/createWalls.jsx
@@ -3,15 +3,7 @@ import { useTexture } from '@react-three/drei';
 
 export default function CreateWalls()
 {
-  const textures = {
-    ...useTexture({
-      map: "./Textures/Metal007_1K-JPG_Color.jpg",
-      roughnessMap: "./Textures/Metal007_1K-JPG_Roughness.jpg",
-      metalnessMap: "./Textures/Metal007_1K-JPG_Metalness.jpg",
-      normalMap: "./Textures/Metal007_1K-JPG_NormalGL.jpg",
-    }),
-    metalness: 0.2
-  };
+
     return <>
       <mesh receiveShadow rotation-x={ Math.PI * 0.5 }>
         <planeGeometry args={[60, 60]}/>
